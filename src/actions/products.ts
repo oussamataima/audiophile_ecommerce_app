582a15e739b43c84190e1e29aca33c5d81cf02b6
aizensosuke <stayhomee0@gmail.com>
2024-07-12 20:52:49 +0100

    add url for deploiment

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -2,13 +2,13 @@
 
 import { Product } from "@/types";
 
-
+const url = process.env.url
 export async function getProducts() {
-    const products = await fetch("http://localhost:3000/data.json",{ cache: 'no-store' }).then((res) => res.json())
+    const products = await fetch(`${url}/data.json`, { cache: 'no-store' }).then((res) => res.json())
     return products
 }
 export async function getProductByCategory(category: string) {
-    const products = await fetch("http://localhost:3000/data.json", { cache: 'no-store' })
+    const products = await fetch(`${url}/data.json`, { cache: 'no-store' })
         .then((res) => res.json())
         .then((data: Product[]) => data
             .filter((product: Product) => product.category === category.toLocaleLowerCase())
@@ -16,10 +16,11 @@ export async function getProductByCategory(category: string) {
     return products
 }
 export async function getProductBySlug(slug: string) {
-    const products = await fetch("http://localhost:3000/data.json", { cache: 'no-store' })
+    const products = await fetch(`${url}/data.json`, { cache: 'no-store' })
         .then((res) => res.json())
         .then((data: Product[]) => data
             .find((product: Product) => product.slug === slug.toLocaleLowerCase()));
     return products
 }
+
 
